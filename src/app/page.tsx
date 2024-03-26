cb32670caeb4717e456f6f1c9f336b519bbae2a1
Ratul <cs.ratul03@gmail.com>
2024-03-26 15:33:29 +0530

    landing page and ShadCN support added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,67 @@
 import Image from "next/image";
+import landing from "../assets/landing.svg";
+import devimg from "../assets/dev-focus.svg";
+import program from "../assets/programming.svg";
+import percent from "../assets/percent.svg";
+import { Button } from "@/components/ui/button";
+import { FaSyncAlt } from "react-icons/fa";
+import { BsChatLeftTextFill } from "react-icons/bs";
+import { IoIosGitMerge } from "react-icons/io";
+import { FaRocket } from "react-icons/fa6";
+import Link from "next/link";
+
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  return (<>
+    <main className="flex bg-gradient-to-tr from-[#070b0a] from-[40%] via-[#287566] to-[#33e8c3] to-[90%] text-[#f0f2f1] min-h-screen items-center justify-around p-16">
+      <div className="w-[50vw]">
+        <h1 className="text-7xl font-semibold py-5">Code & Collaborate</h1>
+        <p className="text-xl break-words py-5">A collaborating platform meant for developers meaning to bring out the best in them.</p>
+        <Button asChild className="bg-[#8de1d0] text-[#070b0a]">
+          <Link href="/login">Explore</Link>
+        </Button>
+      </div>
+      <div className="relative w-full">
+        <div className="absolute bg-[#070b0a] rounded-xl top-0 left-0 w-[50%] bg-opacity-30 backdrop-blur-xl">
+        <Image src={devimg} alt="landing" width={300} height={300} className="m-15"/>
+        </div>
+        <div className="absolute bg-[#070b0a] rounded-xl right-0 bottom-0 w-[50%] bg-opacity-60 backdrop-blur-lg">
+        <Image src={program} alt="landing" width={400} height={400} className="m-15"/>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+    <div className="bg-[#070b0a]">
+        <div className="w-full bg-opacity-80 text-center text-[#f0f2f1] py-20">
+          <h1 className="text-5xl font-semibold pb-5">Struggling with inefficient <span className="bg-gradient-to-r text-transparent bg-clip-text from-[#8de1d0] to-[#287566]">code collaboration?</span></h1>
+          <h2 className="text-2xl px-5 mx-10">Eliminate communication gaps and edit code together in real-time. Supercharge your team's workflow with seamless collaboration on the same codebase. </h2>
+        </div>
+        <div className="grid grid-cols-2 px-10 mx-10 gap-x-10">
+          <div className="pb-10 text-[#f0f2f1]/60 col-span-1">
+            <p className="flex justify-start items-center gap-x-3 text-base py-5 font-semibold "><span className="p-2 bg-[#04251d] w-10 rounded-lg bg-gradient-to-tl from-[#2f55dd] to-[#8aece7]"><FaSyncAlt className="w-6 h-6 bg-clip-text"/></span> <span className="bg-gradient-to-r from-[#33e8c3] to-[#4d5dfb] text-transparent bg-clip-text">Effortless Live Editing</span></p>
+            <p className="text-xs">Stop lagging behind! Edit code simultaneously with your team and see changes update instantly for everyone.</p>
+            <p className="text-xs py-3">No more waiting or version conflicts - work seamlessly on the same codebase, fostering a collaborative flow.</p>
+          </div>
+          <div className="text-[#f0f2f1]/60 col-span-1">
+            <p className="flex justify-start items-center gap-x-3 text-base py-5 font-semibold "><span className="p-2 bg-[#04251d] w-10 rounded-lg bg-gradient-to-tl from-[#2f55dd] to-[#8aece7]"><FaRocket  className="w-6 h-6"/></span> <span className="bg-gradient-to-r from-[#33e8c3] to-[#4d5dfb] text-transparent bg-clip-text">Increased Developer Productivity</span></p>
+            <p className="text-xs">Reduce time spent on coordination and context switching.</p>
+            <p className="text-xs py-3">Boost your team's overall efficiency and accelerate project completion.</p>
+          </div>
+          <div className="text-[#f0f2f1]/60 col-span-1">
+            <p className="flex justify-start items-center gap-x-3 text-base py-5 font-semibold "><span className="p-2 bg-[#04251d] w-10 rounded-lg bg-gradient-to-tl from-[#2f55dd] to-[#8aece7]"><BsChatLeftTextFill  className="w-6 h-6"/></span> <span className="bg-gradient-to-r from-[#33e8c3] to-[#4d5dfb] text-transparent bg-clip-text">Integrated Chat</span></p>
+            <p className="text-xs">Break down communication silos. Chat with your team members directly within the code editor.</p>
+          </div>
+          <div className="col-span-1 row-span-2 flex justify-center items-center">
+            <Image src={percent} alt="features" height={400} width={400}/>
+          </div>
+          <div className="text-[#f0f2f1]/60 col-span-1">
+            <p className="flex justify-start items-center gap-x-3 text-base py-5 font-semibold "><span className="p-2 bg-[#04251d] w-10 rounded-lg bg-gradient-to-tl from-[#2f55dd] to-[#8aece7]"><IoIosGitMerge  className="w-6 h-6"/></span> <span className="bg-gradient-to-r from-[#33e8c3] to-[#4d5dfb] text-transparent bg-clip-text">Version Control Integration</span></p>
+            <p className="text-xs">Track your project's history with ease. View changes made by anyone, at any point.</p>
+            <p className="text-xs py-3">Experiment and iterate safely, knowing you can revert back if necessary.</p>
+          </div>
+
+        </div>
+    </div>
+    </>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,23 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 export default function SignupPage(){
     const router = useRouter();
@@ -37,6 +54,8 @@ export default function SignupPage(){
     },[user])
 
     return (
+        <>
+       
         <div className="flex-col">
             <h1>{loading ? "Processing" : "Signup"}</h1>
             <hr />
@@ -49,5 +68,58 @@ export default function SignupPage(){
             <button type="submit" onClick={onSignup}>{btnDisabled ? "No Signup" : "Signup"}</button>
             <Link href="/login">Already have an account?</Link>
         </div>
+        <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" defaultValue="@peduarte" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+        </>
     )
 }
